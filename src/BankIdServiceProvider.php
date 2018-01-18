6bbaee3777475c827881cfae5a325406403c7eed
Leorent Kelmendi <leorent@hotmail.com>
2018-01-19 00:03:01 +0100

    Fix on service provider

--- src/BankIdServiceProvider.php
+++ src/BankIdServiceProvider.php
@@ -12,12 +12,12 @@ class BankIdServiceProvider extends ServiceProvider
         require __DIR__ . '/routes/routes.php';
     }
 
-    public function register(){
+    public function register()
+    {
 
-
-        $this->app->bind('BankID', function (){
+        $this->app->bind('BankID', function () {
 
             return new BankID;
-        })
+        });
     }
 }
